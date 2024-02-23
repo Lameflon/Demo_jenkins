f033d163dae640ddcd1cba5d032bcf78d383377e
Arun <77752099+Lameflon@users.noreply.github.com>
2024-02-23 12:45:57 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,8 @@
 // This is a simple Java program.
 // FileName : "HelloWorld.java".
  
-public class Main {
-    public static void main 
+public class Main { 
+    public static void main {
     System.out.println("Hey!");
      System.out.println("How are you !?");
     }
